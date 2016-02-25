5f4f7f7d86b8f64c31452822525a2bb98e95d94b
Bing <zhxubing@gmail.com>
2016-02-25 22:27:06 +0800

    delete build file

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTJSCProfiler.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Bing/Desktop/test/RNReduxDemo/node_modules/react-native/React/Profiler/RCTJSCProfiler.m \
-  /Users/Bing/Desktop/test/RNReduxDemo/node_modules/react-native/React/Profiler/RCTJSCProfiler.h \
-  /Users/Bing/Desktop/test/RNReduxDemo/node_modules/react-native/React/Base/RCTDefines.h \
-  /Users/Bing/Desktop/test/RNReduxDemo/node_modules/react-native/React/Base/RCTLog.h \
-  /Users/Bing/Desktop/test/RNReduxDemo/node_modules/react-native/React/Base/RCTAssert.h
